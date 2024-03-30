696b6f5f5d4cb50023feb2fa2d83f7a4af3d0100
Atharva <atharvajadhav70@gmail.com>
2024-03-30 20:17:57 +0530

    tf version 10

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import axios from "axios"
 import { Slide, ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-axios.defaults.baseURL = `http://localhost:5000`
+axios.defaults.baseURL = `http://127.0.0.1:5000`
 
 const App = () =>  {
 
